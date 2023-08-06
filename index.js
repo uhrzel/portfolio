8136ddbe99a275e11a7c2a978eac5aef8546e170
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-06 22:30:20 +0800

    fix overflow

--- index.js
+++ index.js
@@ -3,4 +3,16 @@
 const concatenatedFruit = fruits.join(" ");
 console.log(concatenatedFruit);
  */
+     const links = document.querySelectorAll(".nav-link");
 
+     links.forEach((link) => {
+       link.addEventListener("click", () => {
+         // Remove active class from all links
+         links.forEach((link) => {
+           link.classList.remove("active");
+         });
+
+         // Add active class to the clicked link
+         link.classList.add("active");
+       });
+     });
