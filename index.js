17b8bca946c9158b01796516a81584ae9d14fe35
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-07 14:36:09 +0800

    finish

--- index.js
+++ index.js
@@ -3,16 +3,56 @@
 const concatenatedFruit = fruits.join(" ");
 console.log(concatenatedFruit);
  */
-     const links = document.querySelectorAll(".nav-link");
+const links = document.querySelectorAll(".nav-link");
 
-     links.forEach((link) => {
-       link.addEventListener("click", () => {
-         // Remove active class from all links
-         links.forEach((link) => {
-           link.classList.remove("active");
-         });
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Remove active class from all links
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
 
-         // Add active class to the clicked link
-         link.classList.add("active");
-       });
-     });
+    // Add active class to the clicked link
+    link.classList.add("active");
+  });
+});
+
+let professions = ["Frontend Development", "Digital Marketing", "Email Marketing"];
+let symbols = [
+  "1",
+  "0",
+  "y",
+  "x",
+  "#",
+  "-",
+  "=",
+  "0",
+  "1",
+  "$",
+  "@",
+  ":",
+  ";",
+  "{",
+  "}",
+  "[",
+  "]",
+  "|",
+];
+let professionIndex = 0;
+let is_index = 0;
+let body = document.querySelector(".portfolio");
+
+let interval = setInterval(() => {
+  let currentProfession = professions[professionIndex];
+  if (is_index < currentProfession.length) {
+    let text = currentProfession.slice(0, is_index + 1);
+    if (is_index < currentProfession.length - 1) {
+      text += symbols[Math.floor(Math.random() * symbols.length)];
+    }
+    body.textContent = text;
+    is_index++;
+  } else {
+    professionIndex = (professionIndex + 1) % professions.length;
+    is_index = 0;
+  }
+}, 150);
